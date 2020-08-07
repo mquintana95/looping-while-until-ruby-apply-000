47ac87343053e8033c71e9d880d48f0225fa3d18
Marcus Quintana <mq97325@gmail.com>
2020-08-07 23:09:30 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,7 @@
 def using_until
-  levitation_force = 6
-  #your code here
-    
+  levitation_level = 6
+  while levitation_level
+    puts 'Wingardium Leviosa'
+    levitation_level =+ 1 
+  end
 end
-
